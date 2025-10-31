4b46613025dbc1c80f8b4c288165cf5bb078af4a
mateus vieira <mateuss.vieira.dev@gmail.com>
2025-10-31 17:41:11 -0300

    FichaFitApp

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      className="flex-1 items-center justify-center bg-blue-100"
+    >
+      <Text className="text-xl font-bold text-green-500">Olá teste</Text>
+    </View>
+  );
+}
